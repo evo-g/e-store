987a2d03053fbaa2beee50bdc3721e3f99b161f3
Evodio Garcia <evodiog@gmail.com>
2019-02-01 22:56:26 -0800

    adds new methods to context.js, prop ? for buttonContainers, setup detail page with return to product page,product page adds propTypes

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ProductConsumer } from '../context';
+import PropTypes from 'prop-types'
 
 export default class Product extends Component {
   render() {
@@ -36,7 +37,7 @@ export default class Product extends Component {
               {title}
             </p>
             <h5 className='text-blue font-italic mb-0'>
-              <span className='mr-1'>
+              <span className='mr-1'>$
               </span>
               {price}
             </h5>
@@ -48,6 +49,16 @@ export default class Product extends Component {
     );
   }
 }
+
+Product.propTypes = {
+  product:PropTypes.shape({
+    id: PropTypes.number,
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    inCart: PropTypes.boolean
+  }).isRequired 
+};
 
 const ProductWrapper = styled.div`
   .card {
--- src/context.js
+++ src/context.js
@@ -7,30 +7,73 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state ={
-        products: storeProducts,
+        products: [],
         detailProduct
+    };
+
+    componentDidMount() {
+        this.setProducts();
+    };
+
+    setProducts = () => {
+        let tempProducts = [];
+        storeProducts.forEach(item => {
+            const singleItem = {...item};
+            tempProducts = [...tempProducts, singleItem];
+        })
+        this.setState(() =>{
+            return { products: tempProducts }
+        });
     }
+
+    getItem = (id) => {
+        const product = this.state.products.find(item => item.id === id);
+
+    };
+
     handleDetail = () => {
-        console.log('hello from detail')
+        const product = this.getItem();
+        this.setState(()=> {
+            return { detailProduct: product } 
+        })
     }
-    addToCart = () => {
-        console.log('hello from detail')
+    addToCart = (id) => {
+        console.log(`hello from cart.id is ${id}`)
     }
-  render() {
-    return (
-      <ProductContext.Provider 
-        value={{
-            ...this.state,
-            handleDetail: this.handleDetail,
-            addToCart: this.addToCart 
-        }}
-      >
+
+    render() {
+        return (
+            <ProductContext.Provider 
+            value={{
+                ...this.state,
+                handleDetail: this.handleDetail,
+                addToCart: this.addToCart 
+            }}
+            >
         {this.props.children}
       </ProductContext.Provider>
     )
-  }
 }
+};
 
 const ProductConsumer = ProductContext.Consumer;
 
 export { ProductProvider, ProductConsumer };
+
+// tester = () => {
+//     console.log('State Products :', this.state.products[0].inCart);
+//     console.log('Data Products :', storeProducts[0].inCart);
+
+//     const tempProducts = [...this.state.products];
+//     tempProducts[0].inCart = true;
+//     this.setState(
+//         () => {
+//             return { products: tempProducts };
+//         },
+//         () => {
+//             console.log('State Products :', this.state.products[0].inCart);
+//             console.log('Data Products :', storeProducts[0].inCart);
+//         }
+//     );
+// };
+/* <button onClick={this.tester}>test me</button> */
