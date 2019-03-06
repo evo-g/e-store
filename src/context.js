4ebc53d9bcfa07afd1dc8e6c2061b9f46a905251
Evodio Garcia <evodiog@gmail.com>
2019-03-05 16:19:22 -0800

    adds functionality to cart for totals and increment and decrement also changes default component to display requested path and show an error

--- src/context.js
+++ src/context.js
@@ -8,8 +8,8 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
     state = {
         products: [],
-        detailProduct,
-        cart: storeProducts,
+        detailProduct: detailProduct,
+        cart: [],
         modalOpen: false,
         modalProduct: detailProduct,
         cartSubTotal: 0,
@@ -56,11 +56,11 @@ class ProductProvider extends Component {
             () => {
                 return {
                     products: tempProducts,
-                    cart: [...this.state.cart],
-                    product
-                }
+                    cart: [...this.state.cart,
+                    product]
+                };
             }, () => {
-                console.log(this.state);
+                this.addTotals();
             }
         );
     };
@@ -78,20 +78,80 @@ class ProductProvider extends Component {
         })
     };
 
-    increment = (id) => {
-        console.log('this is increment method');
+    increment = id => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+        this.setState(()=> { return { cart: [...tempCart] } }, ()=> { this.addTotals() });
+
     };
     
-    decrement = (id) => {
-        console.log('this is decrement method');
+    decrement = id => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item => item.id === id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+        product.count = product.count - 1;
+        if(product.count === 0) {
+            this.removeItem(id);
+        } else { 
+            product.total = product.count * product.price;  
+            this.setState(()=> {
+                return { cart: [...tempCart]}
+            }, ()=> {
+                this.addTotals();
+            });
+        }
+
     };
     
-    removeItem = (id) => {
-        console.log('item removed');
+    removeItem = id => {
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !== id);
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(()=> {
+            return {
+                cart: [...tempCart],
+                products: [...tempProducts]
+            }
+        }, ()=> {
+            this.addTotals();
+        })
+
     };
 
     clearCart = () => {
-        console.log('cart has been cleared');
+        this.setState(() => {
+            return { cart: [] };
+        }, () => {
+            this.setProducts();
+            this.addTotals();
+        });
+    };
+
+    addTotals = () => {
+        let subTotal = 0;
+        this.state.cart.map(item => (subTotal += item.total));
+        const tempTax = subTotal * 0.1;
+        const tax = parseFloat(tempTax.toFixed(2));
+        const total = subTotal + tax;
+        this.setState(()=> {
+            return {
+                cartSubTotal: subTotal,
+                cartTax: tax,
+                cartTotal: total
+            };
+        });
     };
 
     render() {
